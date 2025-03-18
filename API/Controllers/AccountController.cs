c109e2efe92c90abc1b56c35b8033d1de50570d0
Christian Camacho <chris.ca.dev@gmail.com>
2025-03-18 16:08:07 -0700

    complete login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
@@ -13,7 +14,7 @@ public class AccountController(DataContext context) : BaseApiController
     [HttpPost("register")]
     public async Task<ActionResult<AppUser>> Register(RegisterDto dto)
     {
-        if (await UserExists(dto.Username)) return BadRequest("Username has taken");
+        if (await UserExists(dto.Username)) return BadRequest(new { message = "Username has taken" });
         using var hmac = new HMACSHA512();
 
 
@@ -29,9 +30,34 @@ public class AccountController(DataContext context) : BaseApiController
         return user;
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto dto)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(UsernameFilter(dto.Username));
+        if (user == null) return Unauthorized(new { message = "Not registered" });
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(dto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+                return Unauthorized(new { message = "invalid credentials" });
+        }
+        return user;
+    }
+
+    private static Expression<Func<AppUser, bool>> UsernameFilter(string username)
+    {
+        username = username.ToLower(); // Normalize input once
+        return user => user.UserName.ToLower() == username;
+    }
+
     private async Task<bool> UserExists(string username)
     {
-        return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
+        return await context.Users.AnyAsync(UsernameFilter(username));
     }
+
 
 }
