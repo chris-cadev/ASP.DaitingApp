859dd334d2821426396fcc892a6a424f8731ffe7
Christian Camacho <chris.ca.dev@gmail.com>
2025-03-24 13:47:46 -0700

    feat(nav) added routes for main components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list.component';
+import { MemberDetailComponent } from './members/member-detail.component';
+import { UsersListComponent } from './explore/users-list.component';
+import { MessagesComponent } from './social/messages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'members', component: MemberListComponent },
+    { path: 'members/:id', component: MemberDetailComponent },
+    { path: 'explore', component: UsersListComponent },
+    { path: 'social/messages', component: MessagesComponent },
+    { path: "**", component: HomeComponent, pathMatch: 'full' },
+];
